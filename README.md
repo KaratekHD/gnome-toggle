c98c8a34ae6084ce76bc9c70e6caaba03b0570c8
?xml <frejaland.47@gmail.com>
2021-05-18 15:51:32 -0600

    Comment some stuff

--- README.md
+++ README.md
@@ -5,10 +5,9 @@ Ever wanted a dark theme in GNOME? Well, now you can have it with this convenien
 To install, clone this repo and use make to install. Make sure to add an SSH key to your profile!
 
 ```shell
-$ git clone https://github.com/foreverxml/gnome-toggle.git 
-$ cd gnome-toggle
-$ make
-$ make install
+git clone https://github.com/foreverxml/gnome-toggle.git 
+cd gnome-toggle
+make && make install
 ```
 
 The extension then must be enabled using GNOME Extensions or the [GNOME Extensions website](https://extensions.gnome.org/local/).
--- extension.js
+++ extension.js
@@ -10,12 +10,12 @@ function enable() {
     this.mainMenu = Main.panel.statusArea['aggregateMenu'].menu;
 
     // create theme menu, according to theme.
-    const color = Util.trySpawn(["dconf", "read", "/org/gnome/desktop/interface/gtk-theme"]);
-    if (color === "Adwaita") {
+    /* var color = Util.trySpawn(["dconf", "read", "/org/gnome/desktop/interface/gtk-theme"]);
+    if(color === "'Adwaita'") {
         this.themeMenu = new PopupMenu.PopupSubMenuMenuItem("Light Appearance", true);
         this.mainMenu.addMenuItem(themeMenu, 8);
         this.themeMenu.icon.icon_name = "weather-clear-symbolic";
-    } else if (color === "Adwaita-dark") {
+    } else if (color === "'Adwaita-dark'") {
         this.themeMenu = new PopupMenu.PopupSubMenuMenuItem("Dark Appearance", true);
         this.mainMenu.addMenuItem(themeMenu, 8);
         this.themeMenu.icon.icon_name = "weather-clear-night-symbolic";
@@ -23,7 +23,11 @@ function enable() {
         this.themeMenu = new PopupMenu.PopupSubMenuMenuItem("Appearance", true);
         this.mainMenu.addMenuItem(themeMenu, 8);
         this.themeMenu.icon.icon_name = "weather-overcast-symbolic";
-    }
+    } */
+    
+    this.themeMenu = new PopupMenu.PopupSubMenuMenuItem("Appearance", true);
+    this.mainMenu.addMenuItem(themeMenu, 8);
+    this.themeMenu.icon.icon_name = "weather-overcast-symbolic";
 
     this.light = new PopupMenu.PopupMenuItem("Light");
     this.light.connect('activate', (item, event) => {
@@ -44,11 +48,12 @@ function enable() {
     this.reset_ornament();
 
     // dotty dots that match theme on startup
-    if (color === "Adwaita") {
+    /* var color = Util.trySpawn(["dconf", "read", "/org/gnome/desktop/interface/gtk-theme"]); // it aint really work without this
+    if (color === "'Adwaita'") {
         this.light.setOrnament(Ornament.DOT);
-    } else if (color === "Adwaita-dark") {
+    } else if (color === "'Adwaita-dark'") {
         this.dark.setOrnament(Ornament.DOT);
-    }
+    } */
 }
 
 function set_theme(theme) {
